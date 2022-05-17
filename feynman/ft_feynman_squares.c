475ae8a75b9a183e91a75167c9692030b1db642c
Axel <lbrt.axel@gmail.com>
2022-05-18 01:35:18 +0200

    Function which finds the number of different squares in a grid of NxN squares as follows

--- feynman/ft_feynman_squares.c
+++ feynman/ft_feynman_squares.c
@@ -6,13 +6,15 @@
 /*   By: rukkyaa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 01:08:31 by rukkyaa           #+#    #+#             */
-/*   Updated: 2022/05/18 01:18:15 by rukkyaa          ###   ########.fr       */
+/*   Updated: 2022/05/18 01:35:09 by rukkyaa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned long	feynman_squares(unsigned int n)
 {
-	if (n == 0 || n == 1)
+	if (n == 0)
+		return (0);
+	else if (n == 1)
 		return (1);
 	return (n * n + feynman_squares(n - 1));
 }
