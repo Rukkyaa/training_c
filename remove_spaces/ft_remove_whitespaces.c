15529218c4ca703cc073dc7f9198158a6d0e2d19
Rukkyaa <lbrt.axel@gmail.com>
2022-06-02 00:44:51 +0200

    Function to remove spaces in a string

--- remove_spaces/ft_remove_whitespaces.c
+++ remove_spaces/ft_remove_whitespaces.c
@@ -14,32 +14,32 @@
 
 size_t	ft_no_space_lenght(char string[])
 {
-	size_t	lenght;
+	size_t	length;
 	size_t	i;
 
 	i = 0;
-	lenght = 0;
+	length = 0;
 	while (string[i] != '\0')
 	{
 		i ++;
-		lenght ++;
+		length ++;
 		if (string[i - 1] == ' ')
 		{
-			lenght --;
+			length --;
 		}
 	}
-	return (lenght);
+	return (length);
 }
 
 char	*ft_remove_whitespaces(const char *str)
 {
 	int		index_no_space;
 	int		i;
-	size_t	lenght;
+	size_t	length;
 	char	*no_space_string;
 
-	lenght = ft_no_space_lenght((char *)str);
-	no_space_string = (char *) malloc(lenght * sizeof(char) + 1);
+	length = ft_no_space_lenght((char *)str);
+	no_space_string = (char *) malloc(length * sizeof(char) + 1);
 	i = 0;
 	index_no_space = 0;
 	while (str[i] != '\0')
