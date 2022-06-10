acc6ad3f6c47d5a914001f4dcc04c6cc01294149
Axel <lbrt.axel@gmail.com>
2022-06-10 18:54:33 +0200

    Function which return the factorial of the given number

--- factorial/ft_factorial.c
+++ factorial/ft_factorial.c
@@ -1,17 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   factorial.c                                        :+:      :+:    :+:   */
+/*   ft_factorial.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rukkyaa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 18:18:58 by rukkyaa           #+#    #+#             */
-/*   Updated: 2022/06/10 18:46:29 by rukkyaa          ###   ########.fr       */
+/*   Updated: 2022/06/10 18:54:01 by rukkyaa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 unsigned long	ft_factorial(unsigned long nb)
 {
-	if (nb == 0)
+	if (nb < 0)
+		return (0);
+	else if (nb == 0)
 		return (1);
 	else
 		return (nb * factorial(nb - 1));
