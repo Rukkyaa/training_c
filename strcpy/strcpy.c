d0c3f22bb8368b3264d2effe8dd6f2874a6b3440
Axel <lbrt.axel@gmail.com>
2022-06-10 13:09:11 +0200

    Function to copy another string

---
+++ strcpy/strcpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strcpy.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rukkyaa <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/10 12:50:16 by rukkyaa           #+#    #+#             */
+/*   Updated: 2022/06/10 13:07:29 by rukkyaa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *string)
+{
+	size_t	len;
+
+	len = 0;
+	while(string[len] != '\0')
+	{
+		len ++;
+	}
+	return (len);
+}
+
+char	*ft_strcpy(const char *string)
+{
+	char	*copy;
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(string);
+	copy = (char *) malloc(len * sizeof(char) + 1);
+	i = 0;
+	while (string[i] != '\0')
+	{
+		copy[i] = string[i];
+		i ++;		
+	}
+	return(copy);
+}
